a1b24133f8dc26d7684a77487e2269af9e3a1a9d
isaque.s <isaque.error404@gmail.com>
2022-10-07 09:49:42 -0300

    refactor: add new props title

--- web/src/components/CardPlay.jsx
+++ web/src/components/CardPlay.jsx
@@ -1,4 +1,4 @@
-export function CardPlay({ timeA, timeB, match }) {
+export function CardPlay({ teamA, teamB, match }) {
   return (
     <div className="rounded-xl border border-gray-300 p-4 text-center space-y-4">
       <span className="text-sm md:text-base text-gray-600 font-bold">
@@ -6,8 +6,8 @@ export function CardPlay({ timeA, timeB, match }) {
       </span>
 
       <div className="flex space-x-4 justify-center items-center">
-        <span className="uppercase">{timeA.slug}</span>
-        <img src={`/assets/flags/${timeA.slug}.png`} alt="" />
+        <span className="uppercase">{teamA.slug}</span>
+        <img src={`/assets/flags/${teamA.slug}.png`} alt="" />
 
         <input
           type="number"
@@ -19,8 +19,8 @@ export function CardPlay({ timeA, timeB, match }) {
           className="bg-red-500/[0.2]  w-[55px] h-[55px] text-red-600 text-xl text-center"
         />
 
-        <img src={`/assets/flags/${timeB.slug}.png`} alt="" />
-        <span className="uppercase">{timeB.slug}</span>
+        <img src={`/assets/flags/${teamB.slug}.png`} alt="" />
+        <span className="uppercase">{teamB.slug}</span>
       </div>
     </div>
   );
